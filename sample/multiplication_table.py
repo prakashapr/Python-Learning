30c4eabb3ca5e4770433f4a8857bccb99d3b9499
Prakasha Pannenahalli Ramalingaiah <prakasha.prp@gmail.com>
2024-07-03 08:11:26 -0700

    multiplication table function

--- sample/multiplication_table.py
+++ sample/multiplication_table.py
@@ -1,6 +1,5 @@
 ##3
-a = 3
-b = 21
+
 # 3 x 1 = 3
 # 3 x 2 = 6
 
@@ -8,6 +7,18 @@ b = 21
 # 3 x 20 = 60
 
 
-# Print the multiplication table for the number 3 up to 20
-for i in range(1,b):
-    print(f"{a} * {i} = {a * i}")
+def multiplication_table(num=1, start=1, end=20):
+    for i in range(start, end+1):
+        print(f"{num} * {i} = {num * i}")
+
+
+a = 10
+b = 90
+c = 11
+multiplication_table(num=a, start=c, end=b)
+
+multiplication_table(num=4, start=20, end=40)
+start_num = 1
+end_num = 20
+for k in range (1, 101):
+    multiplication_table(num=k, start=start_num, end=end_num)
