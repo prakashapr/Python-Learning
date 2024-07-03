7181400b720ac9634e3b6f7369041cf8d9eb4164
Prakasha Pannenahalli Ramalingaiah <prakasha.prp@gmail.com>
2024-07-03 09:09:02 -0700

    student_result

---
+++ sample/student_result.py
@@ -0,0 +1,23 @@
+student_dct = {
+    "A":
+       {
+           "Kannada":90,
+           "english":30
+       },
+
+    "B":
+        {
+            "Kannada": 60,
+            "english": 90
+        },
+    "C":
+        {
+            "Kannada": 50,
+            "english": 40
+        },
+}
+
+### Print result for each student (Pass, Fail %)
+## Print max %
+## print max marks in each subject
+## print pass fail after applying grace number for each subject
