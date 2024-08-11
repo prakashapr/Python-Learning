95e8410573e4827b722b220ddd039236fa958707
Prakasha Pannenahalli Ramalingaiah <prakasha.prp@gmail.com>
2024-08-10 20:33:22 -0700

    csv parsing question

---
+++ sample/csv_parsing.py
@@ -0,0 +1,5 @@
+### (1) read the csv file Python-Learning/files/Data.csv
+### (2) Check "Room Temp" and "Device Temp" are with 5% threshold
+### (3) Value1 will increase when temp cross 55 +/-5%
+### (4) Value2 and Value6 should increase after every 15 +/-5% deg change in temp
+### (5) Value3, Value4, Value5 should increase after every 25 +/-5% dev change in temp. 
